cbe1aa7e785a0f5b557490b72213b178fc487f06
Vino Hoffelder <vinicius.hoffelder1@gmail.com>
2024-02-18 13:23:34 -0300

    Add files via upload

---
+++ desafio1.rb
@@ -0,0 +1,13 @@
+# Cadastro de leitores em uma biblioteca
+
+print "Digite seu nome: "
+name = gets.chomp
+
+print "Digite seu sobrenome: "
+surname = gets.chomp
+
+print "Digite a sua idade: "
+age = gets.chomp.to_i
+
+
+puts "Usuário cadastrado!  nome: #{name} #{surname}, idade: #{age}"
